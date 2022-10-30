d9cad9f81bd1d284095a047df7303b8786ad241e
Piotr Rajtar <piotrek.rajtar@gmail.com>
2022-10-30 23:18:10 +0100

    Add no results info

--- components/cards/cardsList.tsx
+++ components/cards/cardsList.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem, Heading, UnorderedList } from '@chakra-ui/react';
+import { Grid, GridItem, Heading, Text, UnorderedList } from '@chakra-ui/react';
 
 import styles from './cardsList.module.scss';
 import { IngredientRecord } from '../../typings';
@@ -13,17 +13,23 @@ export default function CardsList({ cardsList } : CardListProps) {
   return (
     <section className={styles.section}>
       <Heading className={styles['section-subtitle']} as='h2' size='lg'>INGREDIENTS LIST:</Heading>
-      <UnorderedList listStyleType='none'>
-        <Grid gap={6} templateColumns='repeat(4, 1fr)'>
-          {
-            cardsList.map((card) => (
-              <GridItem key={card.id} bg='teal.400' h='200' rounded='lg' w='100%'>
-                <Card card={card} />
-              </GridItem>
-            ))
-          }
-        </Grid>
-      </UnorderedList>
+      { 
+        !!cardsList.length 
+        ? 
+        <UnorderedList listStyleType='none'>
+          <Grid gap={6} templateColumns='repeat(4, 1fr)'>
+            {
+              cardsList.map((card) => (
+                <GridItem key={card.id} bg='teal.400' h='200' rounded='lg' w='100%'>
+                  <Card card={card} />
+                </GridItem>
+              ))
+            }
+          </Grid>
+        </UnorderedList>
+        : 
+        <Text fontSize='2xl'>No ingredients found</Text>
+      }
     </section>
   )
 }
